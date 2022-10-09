d6f050e8c0a6d7a7af0f1f87b6444ce3ecd8270a
Khalid Bin Walid <kbwalid26@gmail.com>
2022-10-10 00:24:49 +0900

    Revert "stream dump"
    This reverts commit e1ce1a21b153075a42b46ee9c6cb0f5edef8aefb.

--- param.c
+++ param.c
@@ -39,15 +39,6 @@ static bool get_codec_context(AVFormatContext *avctx,
     return true;
 }
 
-static void dump_stream(AVStream *stream) {
-    AVRational time_base = stream->time_base;
-    double time_base_f = time_base.num / (double)time_base.den;
-    printf("Timebase: %d/%d\n", stream->time_base.num, stream->time_base.den);
-    printf("Start time: %ld\n", stream->start_time);
-    printf("Duration: %.2f\n", stream->duration * time_base_f);
-    printf("Number of frames: %ld\n", stream->nb_frames);
-}
-
 bool avparam_init(avparam_t *param, const char *url) {
     int err;
     bool ret;
@@ -73,8 +64,6 @@ bool avparam_init(avparam_t *param, const char *url) {
         LOG_ERROR("No audio/video stream available\n");
         return false;
     }
-    dump_stream(param->avctx->streams[param->video_si]);
-    dump_stream(param->avctx->streams[param->audio_si]);
 
     ret = get_codec_context(param->avctx,
             param->video_si, &param->video_ctx);
